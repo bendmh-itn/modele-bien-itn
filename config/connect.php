768724b3f063b1872aa96c1a01f8158b83da08a6
ben dmh <demab@site.asty-moulin.be>
2022-06-21 11:02:32 +0200

    ajout en prod

--- config/connect.php
+++ config/connect.php
@@ -1,10 +1,11 @@
 <?php
 
+
 try {
     // chaine de connection : informations sur la base de données
-    $strConnection = 'mysql:host=localhost;dbname=immobilier;port=3333';
+    $strConnection = 'mysql:host=oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=vi64zrmqvjdyc1l8';
     // nouvel objet pdo pour appliquer la connection  (chaine, username et password)
-    $pdo = new PDO($strConnection, 'root', 'root', [
+    $pdo = new PDO($strConnection, 'id1nl7ml2e90dl2y', 'b2ipf4dyztakjtd1', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
     ]);
